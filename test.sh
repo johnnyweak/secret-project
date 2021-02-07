36c0636f3e12cc05ff46f45e7c550df3090436d1
Jordan DOULIEZ <jordandouliez@gmail.com>
2021-02-08 00:39:11 +0100

    Fix type

--- test.sh
+++ test.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+
+###
+### KiRameneKoi est hors-scope du CTF !!
+###
+
 echo "Publicité pour les participants du @PHackCTF 2021"
 
 while [[ 1 == 1 ]]; do
   #statements
   curl -s https://kiramenekoi.fr > /dev/null
-  echo "KiRameneKoi est une appli sympa pour s'organiser lors d'apéro, de soirée foot ou de picnic entres potes.'"
+  echo "KiRameneKoi est une appli sympa pour s'organiser lors d'apéros, de soirées foot ou de picnics entre potes.'"
   echo "C'est gratuit. Allez tester! "
   sleep 30
 done
